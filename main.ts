4b47e346e66f1c72489fae4d69c9519c3934ee31
Lineke12 <151532516+Lineke12@users.noreply.github.com>
2023-11-27 15:08:07 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,33 @@
+basic.forever(function () {
+    if ((225 as any) < (135 as any)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if ((135 as any) < (45 as any)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if ((315 as any) < (225 as any)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
